1fd3d1dc38c837989a5a78c648e459b385a007a9
goorockey <kelvingu616@gmail.com>
2014-05-11 14:00:23 -0400

    support multi page archives

--- send_review.js
+++ send_review.js
@@ -2,11 +2,16 @@ var config = require('./config.json');
 var CronJob = require('cron').CronJob;
 var email = require('./lib/email');
 var instapaper = require('./lib/instapaper');
+var render = require('./lib/render');
+var fs = require('fs');
 
 var send_review = function() {
-    instapaper.get_archives(function(items) {
-        email.send(items);
+
+    instapaper.get_archives(function(archives) {
+        var html = render(archives);
+        if (html) email.send(html);
     });
+
 };
 
 try {
